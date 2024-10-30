6b4336b83807cfb13b06b0f421f4474e974f9a78
theManAndHisShadow <justemailpostbox@gmail.com>
2024-10-30 22:35:35 +0500

    [UI]: updated 'items' storing logic.
    Changelog:
    - moved 'UIElementBoundingRect' type to 'global.types';
    - added 'id' property to ui element;
    - now you can get a specific ui element by its id;
    - added 'getElementById()' method.

--- src/ts/ui.ts
+++ src/ts/ui.ts
@@ -1,4 +1,4 @@
-import { Screen } from "./global.types";
+import { Screen, UI, UIElement, UIElementBoundingRect } from "./global.types";
 
 interface UIparams {
     screen: Screen, 
@@ -9,15 +9,8 @@ interface UIparams {
     valueColor?: string, 
 };
 
-interface UIElementBoundingRect {
-    topLeft:     {x: number, y: number},
-    bottomLeft:  {x: number, y: number},
-    topRight:    {x: number, y: number},
-    bottomRight: {x: number, y: number},
-};
 
-
-export default function getUI(params: UIparams) {
+export default function getUI(params: UIparams): UI {
     // Set default values
     const defaultParams: UIparams = {
         screen: params.screen, 
@@ -33,7 +26,7 @@ export default function getUI(params: UIparams) {
     
     const { context } = params.screen;
 
-    const getNewUIelement = function({name, x = 0, y = 0}: {name: string, x: number, y: number}) {
+    const getNewUIelement = function({id, label, x = 0, y = 0}: {id: string, label: string, x: number, y: number}): UIElement {
         // Setting the font before measurements
         context.font = `${params.fontSize}px ${params.fontName}`;  
     
@@ -46,7 +39,7 @@ export default function getUI(params: UIparams) {
         };
     
         // Save the full text of the label
-        let labelText = `${name}:`;
+        let labelText = `${label}:`;
     
         // Getting the sizes of text elements
         let labelRect = context.measureText(labelText);
@@ -60,10 +53,11 @@ export default function getUI(params: UIparams) {
         rect.bottomLeft  = { x: x, y: y + actualHeight + params.lineSpacing };
         rect.topRight    = { x: x + actualWidth, y: y };
         rect.bottomRight = { x: x + actualWidth, y: y + actualHeight + params.lineSpacing };
-    
-        return {
+
+        const newUIElement: UIElement = {
             rect,
-            name,
+            id,
+            label,
             value,
     
             updateValue: (newValue: any) => {
@@ -81,41 +75,48 @@ export default function getUI(params: UIparams) {
                 // Render the element value with the correct labell width offset
                 context.fillStyle = params.valueColor;
                 context.fillText(`${value}`, x + labelRect.width + 3, y); 
-            },
-        };
+            }
+        }; 
+    
+        return newUIElement;
     };
     
 
     const scores = getNewUIelement({
         x: 20, y: 35,
-        name: 'Scores',
+        id: 'scores',
+        label: 'Scroes',
     });
 
-    const time = getNewUIelement({
-        x: scores.rect.bottomLeft.x, 
-        y: scores.rect.bottomLeft.y,
-        name: 'Time',
-    });
+    // const time = getNewUIelement({
+    //     x: scores.rect.bottomLeft.x, 
+    //     y: scores.rect.bottomLeft.y,
+    //     name: 'Time',
+    // });
 
-    const status = getNewUIelement({
-        x: time.rect.bottomLeft.x, 
-        y: time.rect.bottomLeft.y,
-        name: 'Status',
-    });
+    // const status = getNewUIelement({
+    //     x: time.rect.bottomLeft.x, 
+    //     y: time.rect.bottomLeft.y,
+    //     name: 'Status',
+    // });
 
-    const items = {
-        scores: scores,
+    const items = [
+        scores,
 
         // Temporarily disabled
         // time: time,
         // status: status,
-    };
+    ];
 
     return {
         items: items,
 
+        getElementById: (id) => {
+            return items.find(item => item.id === id);
+        },
+
         render: () => {
-            for(let uiElement of Object.values(items)) {
+            for(let uiElement of items) {
                 uiElement.render();
             }
         },
