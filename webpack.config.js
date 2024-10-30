75ae4c445d966c3b9c458202fbe0e3de7546ded1
theManAndHisShadow <justemailpostbox@gmail.com>
2024-10-30 16:55:45 +0500

    [Project]: added basic webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/ts/app.js',
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+
+            {
+                test: /\.css$/, 
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+
+    output: {
+        filename: 'js/bundle.js',
+        path: path.resolve(__dirname, 'build'),
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+        }),
+
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, './src/css'), 
+                    to: path.resolve(__dirname, 'build/css'), 
+                    noErrorOnMissing: true, 
+                },
+            ],
+        }),
+    ],
+};
