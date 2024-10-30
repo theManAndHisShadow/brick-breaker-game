e9db3c941b6df66fa914b9bc32b3315a9e96c935
theManAndHisShadow <justemailpostbox@gmail.com>
2024-10-30 16:03:43 +0500

    [Helpers]: splitted 'getRandomNumber' to two functions - 'getRandomFloat()' and 'getRandomInt()'.

--- js/helpers.js
+++ js/helpers.js
@@ -1,3 +1,10 @@
-function getRandomNumber(min, max) {
+function getRandomFloat(min, max) {
     return Math.random() * (max - min) + min;
+}
+
+
+
+
+function getRandomInt(min, max) {
+    return Math.floor(min + Math.random() * (max + 1 - min));
 }
