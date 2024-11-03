49ca3ac8d7603014a85752845801f987defbab47
theManAndHisShadow <justemailpostbox@gmail.com>
2024-11-03 11:36:22 +0500

    [Globa]: added '.renderAt()' methods to 'Primitive', 'Ball', 'Platform', 'Brick' classes.

--- src/ts/classes/Brick.ts
+++ src/ts/classes/Brick.ts
@@ -1,12 +1,29 @@
 import Primitive from "./Primitive";
-import { BrickType } from "../global.types";
+import { BrickType, ScreenType } from "../global.types";
+
+interface BrickParams {
+    health: number,
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+    color: string;
+}
 
 export default class Brick extends Primitive implements BrickType {
     health: number;
     
-    constructor(params: BrickType){
+    constructor(params: BrickParams){
         super(params);
 
         Object.assign(this, params);
+    }
+
+    renderAt(screenReference: ScreenType): void {
+        const { context } = screenReference;
+        let { x, y, width, height, color } = this;
+    
+        context.fillStyle = color;
+        context.fillRect(x, y, width, height);
     }
 }
--- src/ts/classes/Platform.ts
+++ src/ts/classes/Platform.ts
@@ -1,8 +1,16 @@
 import Primitive from "./Primitive";
-import { PlatformType } from "../global.types";
+import { PlatformType, PrimitiveParams, ScreenType } from "../global.types";
 
 export default class Platform extends Primitive implements PlatformType {
-    constructor(params: PlatformType){
+    constructor(params: PrimitiveParams){
         super(params);
+    }
+
+    renderAt(screenReference: ScreenType): void {
+        const { context } = screenReference;
+        let { x, y, width, height, color } = this;
+    
+        context.fillStyle = color;
+        context.fillRect(x, y, width, height);
     }
 }
