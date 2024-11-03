51a0804e5d78b923058030dfc7c9e85ce2a23437
theManAndHisShadow <justemailpostbox@gmail.com>
2024-11-03 11:11:33 +0500

    [Hepers]: added two new helper functions - 'getArrayLast()', 'changeColorOpacity()'.

--- src/ts/helpers.ts
+++ src/ts/helpers.ts
@@ -4,7 +4,21 @@ export function getRandomFloat(min: number, max: number): number {
 
 
 
-
 export function getRandomInt(min: number, max: number): number {
     return Math.floor(min + Math.random() * (max + 1 - min));
+}
+
+
+
+export function getArrayLast(array: any[]){
+    return array[array.length - 1];
+}
+
+
+
+export function changeColorOpacity(rgba: string, newOpacity: number): string{
+    newOpacity = Math.max(0, Math.min(newOpacity, 1));
+
+    // split to components and change alpha component, then compose and return
+    return rgba.split(', ').map((c, i) => { if(i == 3) c = newOpacity + ')'; return c;}).join(', ');
 }
